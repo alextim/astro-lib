0844205b6593ef89b8e3fc8bbefcb946ba0bbfa6
Alex Tim <buildmnt@gmail.com>
2022-04-28 11:54:31 +0300

    demo

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -6,11 +6,31 @@ export default defineConfig({
   site: 'https://example.com',
   integrations: [
     robotsTxt({
-      sitemap: 'https://aaaa',
-      host: 'some.com',
+      host: 'example.com',
+      sitemap: [
+        'https://example.com/main-sitemap.xml',
+        'https://example.com/images-sitemap.xml'
+      ],
       policy: [
-        { userAgent: '*', crawlDelay: 77, disallow: ['/a', '/b'] },
-        { userAgent: 'gg', allow: ['/c', '/d'] },
+        {
+          userAgent: 'Googlebot',
+          allow: '/',
+          disallow: ['/search'],
+          crawlDelay: 2,
+        },
+        {
+          userAgent: 'OtherBot',
+          allow: ['/allow-for-all-bots', '/allow-only-for-other-bot'],
+          disallow: ['/admin', '/login'],
+          crawlDelay: 2,
+        },
+        {
+          userAgent: '*',
+          allow: '/',
+          disallow: '/search',
+          crawlDelay: 10,
+          cleanParam: 'ref /articles/',
+        },
       ],
     }),
   ],
