019e7e488f6256b4c8c72bcbd7db13664b2523b2
Alex Tim <buildmnt@gmail.com>
2022-05-14 14:49:07 +0300

    refactoring: comments in demo

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig({
       policy: [
         {
           userAgent: '*',
+          // The next line enables or disables the crawling on the `robots.txt` level
           disallow: config.disableIndexing ? '/' : '',
         },
       ],
