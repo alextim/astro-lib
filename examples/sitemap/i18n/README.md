90933c0e4ba50daed2efc42b220733583a537f37
Alex Tim <buildmnt@gmail.com>
2022-06-08 22:31:28 +0300

    docs: readme

--- examples/sitemap/i18n/README.md
+++ examples/sitemap/i18n/README.md
@@ -1,9 +1,9 @@
 # Demo for astro-sitemap integration
 
-This sample demonstrates the localization capabilities of integration.
+This sample demonstrates the localization capabilities of `astro-sitemap` integration.
 
 The `i18n.mjs` file contains the locales basic info.
 
-Its data is used for dynamic page generation, for a language switch and as a source for the `astro-sitemap` integration config.
+Its data is used for dynamic page generation, for language switch and as a source for the `astro-sitemap` integration config.
 
 Play this demo [online](https://stackblitz.com/fork/github/alextim/astro-lib/tree/main/examples/sitemap/i18n).
--- examples/sitemap/i18n/src/i18n.mjs
+++ examples/sitemap/i18n/src/i18n.mjs
@@ -2,19 +2,19 @@ const i18n = {
   defaultLocale: 'en',
   locales: {
     en: {
-      name: 'Webmanifest i18n test',
+      name: 'i18n test',
       short_name: 'Test',
       description: 'This is the application description',
       lang: 'en-US',
     },
     es: {
-      name: 'Prueba Webmanifest i18n',
+      name: 'Prueba i18n',
       short_name: 'Prueba',
       description: 'Esta es la descripción de la aplicación.',
       lang: 'es-ES',
     },
     fr: {
-      name: 'Test i18n du manifeste Web',
+      name: 'Test i18n',
       short_name: 'Test',
       description: "Ceci est la description de l'application",
       lang: 'fr-CA',
